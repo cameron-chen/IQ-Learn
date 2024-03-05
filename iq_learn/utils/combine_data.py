1ab382926c6cf3e076e69452d5721d545d1b31d0
Zichang <2675325968@qq.com>
2024-03-05 16:23:00 +0800

    combine multiple ability levels

--- iq_learn/utils/combine_data.py
+++ iq_learn/utils/combine_data.py
@@ -9,35 +9,44 @@ import matplotlib.pyplot as plt
 import os
 import argparse
 
+def list_of_strings(arg):
+    return arg.split(',')
+
 def main():
     # set up argparser and 
     # read args of data1 and data2 path:
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('data1', type=str, default='experts/HalfCheetah-v3_8.pkl')
-    arg_parser.add_argument('data2', type=str, default='experts/HalfCheetah-v3_6_2327r.pkl')
+    arg_parser.add_argument("data_list", type=list_of_strings, help="List of expert demos to combine")
+    # arg_parser.add_argument('data1', type=str, default='experts/HalfCheetah-v3_8.pkl')
+    # arg_parser.add_argument('data2', type=str, default='experts/HalfCheetah-v3_6_2327r.pkl')
     arg_parser.add_argument('segment_len', type=int, default=5)
     arg_parser.add_argument('save_path', type=str, default='experts/HalfCheetah-v3_Gemini_2k+6k.pkl')
     args = arg_parser.parse_args()
 
-
-
-    with open(args.data1, 'rb') as f:
-        data1 = pickle.load(f)
-    with open(args.data2, 'rb') as f:
-        data2 = pickle.load(f)
+    data = []
+    for i in range(len(args.data_list)):
+        with open(args.data_list[i], 'rb') as f:
+            data.append(pickle.load(f))
+    # with open(args.data1, 'rb') as f:
+    #     data1 = pickle.load(f)
+    # with open(args.data2, 'rb') as f:
+    #     data2 = pickle.load(f)
 
     # Step 2: combine the two files into one file
     # for each file, take three items out of each value list and combine them into a new list
-    def combine_value(data1, data2):
+    def combine_value(data):
         new_data = {}
-        for key in data1.keys():
-            new_data[key] = []
-            new_data[key].extend(data1[key][:args.segment_len])
-            new_data[key].extend(data2[key][:args.segment_len])
+        for key in data[0].keys():
+            for i in range(len(data)):
+                if key not in new_data:
+                    new_data[key] = []
+                new_data[key].extend(data[i][key][:args.segment_len])
         return new_data
 
-    combined_data = combine_value(data1, data2)
+    print(f"Combining data from {args.data_list}")
+    combined_data = combine_value(data)
     # Step 3: save it in experts/ HalfCheetah-v3_Gemini.pkl
+    print(f"Saving combined data to {args.save_path}")
     with open(args.save_path, 'wb') as f:
         pickle.dump(combined_data, f)
 
