60868e571f87c113dc8a76590be1d73910f5d199
Zichang <2675325968@qq.com>
2024-03-18 15:17:09 +0800

    Fixed some bugs related to training

--- iq_learn/dataset/expert_dataset.py
+++ iq_learn/dataset/expert_dataset.py
@@ -41,6 +41,7 @@ class ExpertDataset(Dataset):
         self.cond_dim = cond_dim
         self.cond_type = cond_type
         all_trajectories, perm = load_trajectories(expert_location, num_trajectories, seed)
+        all_trajectories, perm = load_trajectories(expert_location, num_trajectories, seed)
         self.trajectories = {}
 
         # Randomize start index of each trajectory for subsampling
@@ -82,7 +83,7 @@ class ExpertDataset(Dataset):
             # Load data from single file.
             with open(cond_location, 'rb') as f:
                 conds = read_file(cond_location, f)
-        self.conds = conds["emb"]
+        self.conds = conds["emb"][:num_trajectories]
         # print("conds length: ", len(self.conds))
         # print("trajectories length: ", len(self.trajectories["states"]))
         # apply permutation to cond
@@ -157,7 +158,7 @@ def load_trajectories(expert_location: str,
             trajs[k] = [v[i] for i in idx]
     else:
         raise ValueError(f"{expert_location} is not a valid path")
-    return trajs, idx
+    return trajs, perm
 
 
 def read_file(path: str, file_handle: IO[Any]) -> Dict[str, Any]:
